95d95b12603628a39b3203ea2c2805382ed2655e
Kirtana Suresh <42880317+ks3057@users.noreply.github.com>
2020-01-30 08:43:36 -0500

    updated url

--- news-reader-addon.py
+++ news-reader-addon.py
@@ -9,7 +9,7 @@ import requests
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def script_news():
-	    url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=baf6d4e18e8c43a88fc9d2a28e88fc13"
+	    url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=baf6d4e18e8c43a88fc9d2a28e88fc13"
 	    open_bbc_page = requests.get(url).json()
 	    articles = open_bbc_page["articles"]
 	    results = []
