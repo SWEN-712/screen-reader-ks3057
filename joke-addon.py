d3c287da5e0ab382d48c453fec03ad250a985bf6
Kirtana Suresh <42880317+ks3057@users.noreply.github.com>
2020-01-30 08:50:40 -0500

    updated joke addon

--- joke-addon.py
+++ joke-addon.py
@@ -1,18 +1,19 @@
 import globalPluginHandler
-import tones # We want to hear beeps.
+import tones
 import ui
+from random import randrange
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def script_doBeep(self, gesture):
-		# d = {1:"A perfectionist walked into a bar…apparently, the bar wasn’t set high enough.",
-		# 2:"I ate a clock yesterday, it was very time-consuming.",
-		# 3:"Did you hear about the crook who stole a calendar? He got twelve months.",
-		# 4:"Did you hear about the semi-colon that broke the law? He was given two consecutive sentences.",
-		# 5:"I woke up this morning and forgot which side the sun rises from, then it dawned on me."}
+		d = {1:"A perfectionist walked into a bar…apparently, the bar wasn’t set high enough.",
+		2:"I ate a clock yesterday, it was very time-consuming.",
+		3:"Did you hear about the crook who stole a calendar? He got twelve months.",
+		4:"Did you hear about the semi-colon that broke the law? He was given two consecutive sentences.",
+		5:"I woke up this morning and forgot which side the sun rises from, then it dawned on me."}
 
-		ui.message("hi")
-		# tones.beep(440, 1000)  # Beep a standard middle A for 1 second.
+		var = randrange(1, len(d) + 1)
+		ui.message(d[var])
 
 	__gestures={
 		"kb:NVDA+A": "doBeep"
